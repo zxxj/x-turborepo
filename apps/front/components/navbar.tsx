5e014f3e16dc80aed5240f8cc50648531d06be64
zhangxinxin <1285367184@qq.com>
2025-12-28 22:30:30 +0800

    feat(navbar): 导航栏&暗黑模式

---
+++ apps/front/components/navbar.tsx
@@ -0,0 +1,56 @@
+import ModeToggle from './mode-toggle';
+import { Button } from './ui/button';
+import { House, BookOpenText, LogIn } from 'lucide-react';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
+
+const Navbar = () => {
+  return (
+    <div className="w-full h-14 border-b flex items-center justify-between px-5">
+      <div>logo</div>
+      <div className="flex">
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button variant="ghost">
+              <House /> home
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>首页介绍哈哈哈哈</p>
+          </TooltipContent>
+        </Tooltip>
+
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button variant="ghost">
+              <BookOpenText />
+              articles
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>一些文章哈哈哈</p>
+          </TooltipContent>
+        </Tooltip>
+
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button variant="ghost">
+              <LogIn />
+              sigIn
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>快去登录哈哈</p>
+          </TooltipContent>
+        </Tooltip>
+
+        <ModeToggle />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
