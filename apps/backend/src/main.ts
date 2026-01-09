e919f6406f95b740a82893f95fc9e2526d694f84
zhangxinxin <1285367184@qq.com>
2026-01-09 19:45:35 +0800

    chore(cors): 允许跨域

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -5,6 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  app.enableCors({
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 9999);
 }
 bootstrap();
