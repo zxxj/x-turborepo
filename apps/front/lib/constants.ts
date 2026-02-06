5c17894e74573b7cefc958b63b690175764e296e
zhangxinxin <1285367184@qq.com>
2026-02-06 10:38:58 +0800

    fix(version): next更新至16.1.6版本,react更新至19.2.4版本,react-dom更新至19.2.4版本.

--- apps/front/lib/constants.ts
+++ apps/front/lib/constants.ts
@@ -1,2 +1,2 @@
-export const BACKEND_URL = 'http://localhost:9999';
-// export const BACKEND_URL = 'http://8.130.131.133:9999';
+// export const BACKEND_URL = 'http://localhost:9999';
+export const BACKEND_URL = 'http://8.130.131.133:9999';
